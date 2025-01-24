ed5de07e4700d8cd4a06070100614e8becf92a1b
SufficientlyPositive <62806641+SufficientlyPositive@users.noreply.github.com>
2025-01-24 21:56:17 +0000

    You can now move between scenes
    Basic menu functionality implemented for all scenes, you can navigate between them easily.

--- Assets/Scenes/Game/game_main.gd
+++ Assets/Scenes/Game/game_main.gd
@@ -2,11 +2,27 @@ class_name GameMain
 extends Node
 
 @onready var root_node: Main = get_tree().root.get_child(0) as Main
+@export var game_scene: GameScene
+@export var game_ui: GameUI
 
 func _ready():
 	if not is_instance_valid(root_node):
 		push_error("Root node did not successfully instantiate in GameMain. Unable to quit to main menu.")
+	if not is_instance_valid(game_scene):
+		push_error("GameScene node did not successfully instantiate in GameMain. No game to play!")
+	if not is_instance_valid(game_ui):
+		push_error("GameUI node did not successfully instantiate in GameMain. Cannot access UI.")
+
+func toggle_pause():
+	get_tree().paused = not get_tree().paused
+	game_ui.toggle_pause_menu()
 
 func quit_to_main_menu():
 	root_node.load_main_menu()
-	root_node.remove_game_main()
+	root_node.remove_game_main() # queue free's self
+	get_tree().paused = false
+
+# all input handling related non-gameplay actions should be handled here!
+func _process(_delta):
+	if Input.is_action_just_pressed("EscapeKeyPressed"):
+		toggle_pause()
--- Assets/Scenes/Game/game_ui.gd
+++ Assets/Scenes/Game/game_ui.gd
@@ -1,2 +1,14 @@
 class_name GameUI
 extends Node
+
+@export var in_game_ui: Control # placeholder
+@export var pause_menu: Control
+
+func _ready():
+	if not is_instance_valid(in_game_ui):
+		push_error("In game UI not instantiated in GameUI. Recipes and the like will not be available.")
+	if not is_instance_valid(pause_menu):
+		push_error("Pause Menu not instantiated in GameUI. Will not be able to exit to menu and pressing 'esc' will cause a crash.")
+
+func toggle_pause_menu():
+	pause_menu.visible = not pause_menu.visible
