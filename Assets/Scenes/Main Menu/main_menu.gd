661d9c223a81d9e5ddabb81af30d969f7a72569e
SufficientlyPositive <62806641+SufficientlyPositive@users.noreply.github.com>
2025-01-24 20:42:03 +0000

    Main menu now working as intended.

--- Assets/Scenes/Main Menu/main_menu.gd
+++ Assets/Scenes/Main Menu/main_menu.gd
@@ -1,7 +1,15 @@
 class_name MainMenu
 extends Control
 
-var _on_start_pressed: Callable
+var on_start_pressed: Callable
+var on_quit_pressed: Callable
+
+func initialise(on_start_pressed: Callable, on_quit_pressed: Callable):
+	self.on_start_pressed = on_start_pressed
+	self.on_quit_pressed = on_quit_pressed
 
 func _on_start_button_pressed():
-	_on_start_pressed.call()
+	on_start_pressed.call()
+
+func _on_quit_button_pressed():
+	on_quit_pressed.call()
--- Assets/Scenes/Main/main.gd
+++ Assets/Scenes/Main/main.gd
@@ -1,5 +1,5 @@
 class_name Main
-extends Node2D
+extends Node
 
 const main_menu_scene = preload("res://Assets/Scenes/Main Menu/main_menu.tscn")
 #const game_node = TODO put stuff here.
@@ -8,8 +8,11 @@ var main_menu_root_node: MainMenu
 
 func _ready():
 	main_menu_root_node = main_menu_scene.instantiate() as MainMenu
-	main_menu_root_node._on_start_pressed = self.start as Callable
+	main_menu_root_node.initialise(self.start, self.exit)
 	self.add_child(main_menu_root_node)
 
 func start():
 	pass
+
+func exit():
+	get_tree().quit()
