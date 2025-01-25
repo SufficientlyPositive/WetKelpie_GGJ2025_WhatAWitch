6e2e6c833adb794892a8f1c23f7843e870b765c7
Dillon Aitken <dillonaitken87@gmail.com>
2025-01-25 12:15:34 +0000

    Ingredience Enoom

---
+++ Assets/Scenes/Game/recipe_manager.gd
@@ -0,0 +1,22 @@
+class_name RecipeManager
+extends TextureRect
+
+enum Ingredients {
+		SNAKE_EYES,
+		FROGS_LEGS,
+		GEMSTONE,
+		NIGHTSHADE,
+		TOADSTOOL,
+}
+
+class Recipe:
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
