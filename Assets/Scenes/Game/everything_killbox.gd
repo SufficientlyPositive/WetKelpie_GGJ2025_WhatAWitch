e6da5b8e06d4f3c95fc56f03de440c4f12c3cbc1
Maxim <maxdespinoy@gmail.com>
2025-01-25 13:46:54 +0000

    Added external killbox

---
+++ Assets/Scenes/Game/everything_killbox.gd
@@ -0,0 +1,19 @@
+class_name EverythingKillbox
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_killbox_entered(body: Node2D) -> void:
+	if body is PlayerCharacter:
+		pass
+	#elif body is Ingredient:
+		#body.queue_free()
+	else:
+		body.get_parent().queue_free()
