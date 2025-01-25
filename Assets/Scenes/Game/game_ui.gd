2f298bbb4e558471b696ba347a7bd89876fe461c
Dillon Aitken <dillonaitken87@gmail.com>
2025-01-25 15:47:51 +0000

    recipee list

--- Assets/Scenes/Game/game_ui.gd
+++ Assets/Scenes/Game/game_ui.gd
@@ -6,7 +6,8 @@ extends Node
 
 func _ready():
 	if not is_instance_valid(in_game_ui):
-		push_error("In game UI not instantiated in GameUI. Recipes and the like will not be available.")
+		pass
+		#push_error("In game UI not instantiated in GameUI. Recipes and the like will not be available.")
 	if not is_instance_valid(pause_menu):
 		push_error("Pause Menu not instantiated in GameUI. Will not be able to exit to menu and pressing 'esc' will cause a crash.")
 
--- Assets/Scenes/Game/recipe_manager.gd
+++ Assets/Scenes/Game/recipe_manager.gd
@@ -1,22 +1,121 @@
 class_name RecipeManager
-extends TextureRect
+extends NinePatchRect
+
+
+var ingredient_sprites = [
+	preload("res://Assets/Images/Programmer Art/ingredient_snake_eyes.png"),
+	preload("res://Assets/Images/Programmer Art/ingredient_frogslegs.png"),
+	preload("res://Assets/Images/Programmer Art/ingredient_nightshade.png"),
+	preload("res://Assets/Images/Programmer Art/ingredient_mushroom.png"),
+	preload("res://Assets/Images/Programmer Art/ingredient_gemstone.png"),
+]
+var recipe_card = preload("res://Assets/Scenes/Game/recipe_card.tscn")
+
 
 enum Ingredients {
 		SNAKE_EYES,
 		FROGS_LEGS,
-		GEMSTONE,
 		NIGHTSHADE,
 		TOADSTOOL,
+		GEMSTONE,
 }
 
 class Recipe:
-	pass
+	var ingredients : Array[Ingredients]
+	var value : int
+	
+	func _init(ing : Array[Ingredients], val : int) -> void:
+		ingredients = ing
+		value = val
+	
+
+var valid_recipes : Array[Recipe] = [
+	Recipe.new([
+			Ingredients.SNAKE_EYES,
+			Ingredients.SNAKE_EYES,
+			Ingredients.SNAKE_EYES],
+			100),
+	Recipe.new([
+			Ingredients.FROGS_LEGS,
+			Ingredients.FROGS_LEGS,
+			Ingredients.FROGS_LEGS],
+			100),
+	Recipe.new([
+			Ingredients.NIGHTSHADE,
+			Ingredients.NIGHTSHADE,
+			Ingredients.NIGHTSHADE],
+			100),
+	Recipe.new([
+			Ingredients.TOADSTOOL,
+			Ingredients.TOADSTOOL,
+			Ingredients.TOADSTOOL],
+			100),
+	Recipe.new([
+			Ingredients.GEMSTONE,
+			Ingredients.GEMSTONE,
+			Ingredients.GEMSTONE],
+			100),
+	Recipe.new([
+			Ingredients.FROGS_LEGS,
+			Ingredients.NIGHTSHADE,
+			Ingredients.GEMSTONE],
+			100),
+	Recipe.new([
+			Ingredients.SNAKE_EYES,
+			Ingredients.FROGS_LEGS,
+			Ingredients.GEMSTONE],
+			100),
+	Recipe.new([
+			Ingredients.FROGS_LEGS,
+			Ingredients.GEMSTONE,
+			Ingredients.TOADSTOOL],
+			100),
+	Recipe.new([
+			Ingredients.GEMSTONE,
+			Ingredients.FROGS_LEGS,
+			Ingredients.NIGHTSHADE],
+			100),
+	Recipe.new([
+			Ingredients.TOADSTOOL,
+			Ingredients.FROGS_LEGS,
+			Ingredients.GEMSTONE],
+			100),
+	Recipe.new([
+			Ingredients.NIGHTSHADE,
+			Ingredients.TOADSTOOL,
+			Ingredients.SNAKE_EYES],
+			100),
+	Recipe.new([
+			Ingredients.TOADSTOOL,
+			Ingredients.FROGS_LEGS,
+			Ingredients.GEMSTONE],
+			100),
+]
+
+var current_recipe : Recipe
+
+
+func pick_new_current_recipe():
+	if $MarginContainer/CurrentRecipe.get_child_count() > 0:
+		$MarginContainer/CurrentRecipe.get_child(0).queue_free()
+	current_recipe = valid_recipes.pick_random()
+	var rc = recipe_card.instantiate()
+	for i in range(3):
+		rc.get_node("I" + str(i)).texture = ingredient_sprites[int(current_recipe.ingredients[i])]
+		rc.get_node("Val").text = str(current_recipe.value)
+	$MarginContainer/CurrentRecipe.add_child(rc)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for r in valid_recipes:
+		var rc = recipe_card.instantiate()
+		for i in range(3):
+			rc.get_node("I" + str(i)).texture = ingredient_sprites[int(r.ingredients[i])]
+		rc.get_node("Val").text = str(r.value)
+		$MarginContainer/RecipeCards.add_child(rc)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
