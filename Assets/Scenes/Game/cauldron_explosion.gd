d60bf41fcb32b6b90ea370ca68132b679a27fffa
Maxim <maxdespinoy@gmail.com>
2025-01-26 12:11:09 +0000

    Cauldron now explodes more than once!

--- Assets/Scenes/Game/cauldron_explosion.gd
+++ Assets/Scenes/Game/cauldron_explosion.gd
@@ -1,13 +1,11 @@
 class_name CauldronExplosionAnimation
 extends AnimatedSprite2D
 
-const no_anim_name = "default"
 const explosion_anim_name = "explosion"
 
 func explode() -> void:
-	if animation == no_anim_name:
-		play(explosion_anim_name)
+	visible = true
+	play(explosion_anim_name)
 
 func on_animation_finished() -> void:
-	if animation == explosion_anim_name:
-		play(no_anim_name)
+	visible = false
