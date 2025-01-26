ca45a617b64792212c8b688684a2d3641d3d8d7a
Maxim <maxdespinoy@gmail.com>
2025-01-26 14:38:01 +0000

    updated bubble control with new api and now bubble button works :)

--- Assets/Scenes/Bubble/bubble_control.gd
+++ Assets/Scenes/Bubble/bubble_control.gd
@@ -6,17 +6,24 @@ const momentum_factors : Vector2 = Vector2(10.0, 1.0)
 var player_reference: PlayerCharacter
 var bubble_scene
 
+const bubble_throttle: float = 2.0
+var time_until_next_bubble: float = 0
+
 func _ready():
 	# Access Parent scene first in order to have access to the unique name
 	player_reference = get_parent().get_node("%PlayerCharacter")
 	bubble_scene = preload("res://Assets/Scenes/Bubble/bubble.tscn")
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("Jump") and player_reference.is_on_floor() and not player_reference.cauldron_exploding:
-		var acceleration : Vector2 = Vector2(player_reference.get_x_accel(delta), player_reference.get_y_accel(delta))
-		acceleration = acceleration * momentum_factors
-		#print("Player jumped! Releasing Bubble with momentum: ", acceleration.x, " ", acceleration.y)
-		var bubble = bubble_scene.instantiate()
-		add_child(bubble)
-		bubble.velocity = acceleration
-		bubble.position = player_reference.position
+	time_until_next_bubble -= delta
+	if Input.is_action_pressed("Bubble") and (time_until_next_bubble < 0) and not player_reference.cauldron_exploding:
+		spawn_bubble(player_reference.position, Vector2(player_reference.get_x_accel(delta), player_reference.get_y_accel(delta)))
+		time_until_next_bubble = bubble_throttle
+
+func spawn_bubble(pos: Vector2, acceleration: Vector2):
+	acceleration = acceleration * momentum_factors
+	#print("Player jumped! Releasing Bubble with momentum: ", acceleration.x, " ", acceleration.y)
+	var bubble = bubble_scene.instantiate()
+	add_child(bubble)
+	bubble.velocity = acceleration
+	bubble.position = pos
